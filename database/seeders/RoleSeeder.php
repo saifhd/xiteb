973cfafd321b5e34c42d5b0988ec74a31422948c
Saiful Islam <saifhd85@gmail.com>
2022-03-21 17:50:58 +0530

    setting roles to users table

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'staff'
+        ]);
+
+        Role::create([
+            'name' => 'user'
+        ]);
+
+        Role::create([
+            'name' => 'admin'
+        ]);
+
+    }
+}
